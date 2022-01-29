b56a9476fa5d852fc75eb415e7b0df564cbf5728
Anik <senanik45@gmail.com>
2022-01-29 20:04:57 +0530

    creating auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path:"",redirectTo:"auth",pathMatch:"full"},
+ {path:"auth",loadChildren: () => import('./auth/auth.module').then(mod => mod.AuthModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
